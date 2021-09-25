ec26c1fe0b8b10718e9bf9aa0f581b7ba0010db3
yangguan <438304@163.com>
2021-09-25 08:28:08 +0800

    update

---
+++ magisoul-server/magisoul-system/src/main/java/org/magisoul/system/entity/SysResourceInfo.java
@@ -0,0 +1,17 @@
+package org.magisoul.system.entity;
+
+import java.io.Serializable;
+
+public class SysResourceInfo implements Serializable {
+
+    private Long id ;
+    private String resourceName ;
+    private String resourceCode ;
+    private Integer level ;
+    private String type ;
+    private Long parentId ;
+    private String requestUrl ;
+    private String enableStatus ;
+    private String isDeleted ;
+
+}
